3674fd872cdb24c649e6ce4ab6bebc9b38d76b80
yanto <62498076+yamacrypt@users.noreply.github.com>
2021-06-05 15:27:36 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup,
+setup(
+    name='tasks',
+    version='0.1.0',
+    license='proprietary',
+    description='Module Experiment',
+    packages=find_packages(where='./'),
+    package_dir={'': './'},
+)
